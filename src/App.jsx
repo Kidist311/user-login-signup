be1778e26f8b7b3e9ca2a2b4f78a99a988dc1b4a
kidist311 <alemayehukidist09@gmail.com>
2025-08-19 12:38:59 +0300

    change made

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+ return (
+  <div>
+    <p>hello world</p>
+  </div>
+ );
+}
+//<img src={armorImg} alt="Armor" className="object-cover h-full w-full" />
+export default App;
+//<img className=" ml-4  mb-2 -mt-9 size-16 object-cover rounded-full " src="/Images/armor.jpg" alt="armor" />
+//<img src={armorImg} alt="Armor" className="object-cover h-full w-full" />
